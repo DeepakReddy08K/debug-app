19945262f00a3ffde15f1f330f13f423426a0000
DeepakReddy08K <kondakindideepakreddy@gmail.com>
2026-07-01 11:44:07 +0530

    change local postgress connection to neon cloud postgress connection for deploying

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,10 @@
-// Database configuration — creates a connection pool to PostgreSQL
-// Pool manages multiple connections automatically, better than single Client for web apps
+//First started with local postgress but during deployment we need a live servar so used neon database for further tasks
+//we need to configure SSL for neon cunnection this makes data transfer from neon and backend server encrypted and neon fails to connect without ssl configuration
 
 import pg from 'pg';
 import dotenv from 'dotenv';
 import log from './logger.js';
 
-// Load environment variables from .env file
 dotenv.config();
 
 const { Pool } = pg;
@@ -13,9 +12,10 @@ const { Pool } = pg;
 const pool = new Pool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
-  user: process.env.DB_USER,   
+  user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME, 
+  database: process.env.DB_NAME,
+  ssl: { rejectUnauthorized: false }//required for neon cloud postgress
 });
 
 pool.connect()
